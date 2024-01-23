4a067609f368713708acdd7337a3ed0344eb88a9
HoneyBee <qjrm1430@naver.com>
2024-01-23 17:43:26 +0900

    first edit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ormConfig } from '../orm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [TypeOrmModule.forRootAsync({ useFactory: ormConfig }), CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/cats/cats.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Cat } from './entities/cat.entity';
+
+@Injectable()
+export class CatsService {
+  constructor(
+    @InjectRepository(Cat)
+    private catRepository: Repository<Cat>,
+  ) {}
+
+  async create(createCatDto: CreateCatDto) {
+    return await this.catRepository.save(createCatDto);
+  }
+
+  async findOne(id: number) {
+    return await this.catRepository.findOne({ where: { id } });
+  }
+
+  async update(id: number, updateCatDto: UpdateCatDto) {
+    const cat = await this.findOne(id);
+    if (!cat) {
+      throw new Error('cat not found');
+    }
+    Object.assign(cat, updateCatDto);
+    return await this.catRepository.save(cat);
+  }
+
+  async remove(id: number) {
+    const cat = await this.findOne(id);
+    if (!cat) {
+      throw new Error('cat not found');
+    }
+    return await this.catRepository.remove(cat);
+  }
+}
